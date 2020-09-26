146de118c8290d609a9b726059089eb8b5e7e7fc
Jonathan Rasmusson <rasmus4200@gmail.com>
2020-09-26 09:36:33 -0600

    8ball code start

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by jrasmusson on 2020-09-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
